4fc3fc51def8cb7ecdfe51432d6c7b5be6ce60e5
colin jaworski <colinrjaworski.84@gmail.com>
2022-02-26 11:42:40 -0600

    added components for each page and installed react-router-dom

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,40 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import { HashRouter as Router, Route } from 'react-router-dom';
+
 
 function App() {
 
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Feedback!</h1>
-        <h4>Don't forget it!</h4>
-      </header>
-    </div>
+    <Router>
+      <div className='App'>
+        <header className='App-header'>
+          <h1 className='App-title'>Feedback!</h1>
+          <h4>Don't forget aboud it!</h4>
+        </header>
+        <main>
+          <Route path='exact/' >
+            <FeelingForm />
+          </Route>
+          <Route path='/UnderstandingForm' exact>
+            <UnderstandingForm />
+          </Route>
+          <Route path='/SupportForm' exact>
+            <SupportForm />
+          </Route>
+          <Route path='/CommentsForm' exact>
+            <CommentsForm />
+            <Route path='/Review' exact>
+              <Review />
+            </Route>
+            <Route path='/ThankYou' exact>
+              <ThankYou />
+            </Route>
+          </Route>
+        </main>
+      </div>
+    </Router>
   );
 }
 
